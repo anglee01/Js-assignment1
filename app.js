7b70a6a45a1013a4dd042fd08e999276545ae321
ANGLEE <angleearif@gmail.com>
2023-09-07 10:25:48 -0700

    two files added

---
+++ app.js
@@ -0,0 +1,125 @@
+//chapter1 alert
+//q1
+//firstname
+ var firstName="Anglee";
+alert(firstName);
+ 
+//lastname
+var lastName= "Arif";
+alert(lastName);
+
+//email
+var email= "angleearif@gmail.com";
+alert(email);
+
+//phone number
+var phone_number= 30060345;
+alert(phone_number);
+
+//password
+var password= 12345;
+alert(password);
+
+//q2
+//correct code
+alert("You're learning Javascript!");
+ 
+//alert message
+alert("This is my javascript assignment. Hope you like it");
+
+//chapter2 variable for strings
+//q1
+var userName;
+//q2
+var book;
+book= "Jane Eyre";
+console.log(book);
+
+//q3
+var teamName= "Royal tigers";
+alert("My team:"+" "+ teamName);
+
+//q4
+var bestMan="Charlie";
+bestMan= "Potter";
+console.log(bestMan);
+
+//chapter3 variables for numbers
+// q1 decalaration
+var caseQty;
+// q2 initialization
+caseQty=144;
+// q3 second variable
+var num= 9;
+//q4 sum
+var sum;
+sum= caseQty+num;
+console.log("The sum of " +" "+caseQty +" "+ "and"+ " "+"num"+"is:"+" "+ sum );
+
+//q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log("The total order is:"+" "+orderTotal);
+
+//q6
+var value1=12;
+value1= value1+ 10;
+console.log("The new value is: " +" "+ value1);
+
+//chapter4 (Variable names Legal and Illegal)
+ //q1
+//correct statement
+var product_cost = 3.45;
+//q2 camel case 
+var nameOfband;
+//q3
+//declaration of legal variable
+var number_of_tries= 5;
+//q4
+var firstname= "Anglee";
+var lastName="Arif";
+var fullName= firstName+" "+lastName;
+console.log(fullName);
+
+//q5
+// illegal variables
+//  var 2num;
+//  var -result;
+// var int;
+//  var goat 1;
+//  var double;
+
+//q5
+//legal variables
+//  var num1;
+//  var _result;
+//  var birthMonth;
+//  var $myBalance;
+//  var $my_emailAddress;
+
+//chapter5 (Math Expression I)
+//q1
+console.log("The modulus is the arithmetic opertor that gives you the remainder when one number is divided by another?");
+//q2
+var num = 20 % 6;
+console.log(num);
+//q3
+var largeNum=1000*2000;
+console.log(largeNum);
+//q4
+var value1=14;
+var value2=20;
+var finalValue= value1-value2;
+console.log("The diiference between"+" "+  value1+" "+ "and "+" "+value2+" "+"is"+" "+finalValue);
+//q5
+var number1=36;
+var number2=8;
+var rem;
+rem=number1%number2;
+console.log("The remainder of"+" "+number1 +" "+"and"+ " "+ number2+" "+"is" + " "+rem);
+//q6
+var firstTerm=7;
+var secondTerm=9;
+var product= firstTerm*secondTerm;
+alert("The product of"+" "+ firstTerm + " "+"and"+" "+ secondTerm +" "+"is"+" "+product);
